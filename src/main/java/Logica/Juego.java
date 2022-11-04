35bf1d2452bafc91aca0e7ae1c3779db9ca3bf17
Santiago González <santiago.galvez@correounivalle.edu.co>
2022-11-04 12:01:24 -0500

    Creación de paquetes y clases

---
+++ src/main/java/Logica/Juego.java
@@ -0,0 +1,18 @@
+/*
+ * MINIPROYECTO #2
+ *
+ * INTEGRANTES: 
+ * John Freddy Belalcázar
+ * Hernán David Cisneros
+ * Santiago González Gálvez
+ *
+ * Grupo 01 FPOE 
+ *
+ * PROFESOR: Luis Yohany Romo Portilla 
+ */
+
+package Logica;
+
+public class Juego {
+    
+}
