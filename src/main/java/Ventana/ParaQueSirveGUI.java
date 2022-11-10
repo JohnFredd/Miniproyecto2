d6cbf2fa4b70d8cd7350180663455a4a2c71c28d
John Freddy <john.freddy.belalcazar@correounivalle.edu.co>
2022-11-09 19:03:34 -0500

    para que sirve correccion tamaño txtarea

--- src/main/java/Ventana/ParaQueSirveGUI.java
+++ src/main/java/Ventana/ParaQueSirveGUI.java
@@ -29,12 +29,11 @@ public class ParaQueSirveGUI extends PlantillaGUI{
         //CONFIGURACION SOBRE PLANTILLA
         ImageIcon titulo = new ImageIcon("ParaQueSirve.png");
         lblTitulo.setIcon(new ImageIcon(titulo.getImage().getScaledInstance(lblTitulo.getWidth(), lblTitulo.getHeight(), Image.SCALE_SMOOTH)));
-        txtAInfo.setBounds(150, 165, 550, 270);
+        txtAInfo.setBounds(215, 220, 420, 150);
         txtAInfo.append("\n    Un juego que te permite entrenar tu capacidad de\n");
         txtAInfo.append("    codificación y recuerdo de información visual y espacial.\n");
         txtAInfo.append("    Este tipo de habilidades es relevante en la práctica de\n");
         txtAInfo.append("    algunos deportes, el reconocimiento de caras o para\n");
         txtAInfo.append("    recordar dónde guardamos nuestras cosas.");
-        txtAInfo.setAlignmentX(CENTER_ALIGNMENT);
     }
 }
